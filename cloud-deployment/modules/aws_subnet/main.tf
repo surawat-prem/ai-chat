206d80eb1adbfb7b77a59c8c09f70a826a81dd10
surawat-prem <tra.sruawat@gmail.com>
2024-09-17 14:31:41 +0700

    add resource var

--- cloud-deployment/modules/aws_subnet/main.tf
+++ cloud-deployment/modules/aws_subnet/main.tf
@@ -2,5 +2,6 @@ resource "aws_subnet" "main" {
     for_each = var.aws_subnets
     vpc_id = var.vpc_id
     cidr_block = each.value["cidr_block"]
+    availability_zone = each.value["availability_zone"]
     tags = each.value["aws_subnet_tag"]
 }
