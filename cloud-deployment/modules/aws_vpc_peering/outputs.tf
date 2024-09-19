db2d2edba5f4abd7d38675e0bbf6bfd9a59c822f
surawat-prem <tra.sruawat@gmail.com>
2024-09-19 18:46:35 +0700

    remove unused outputs

--- cloud-deployment/modules/aws_vpc_peering/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vpc_id" {
-    value = { for k, vpc in aws_vpc.main : k => vpc.id }
-}
-
-output "vpc_net_gw_id" {
-    value = { for k, gateway in aws_internet_gateway.main : k => gateway.id }
-}
