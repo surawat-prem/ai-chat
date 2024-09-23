bfbb3e7328cf9709e78125b9b9e57d3a30ce4638
surawat-prem <tra.sruawat@gmail.com>
2024-09-24 02:48:01 +0700

    fix missing var

--- cloud-deployment/network-workspace/variables.tf
+++ cloud-deployment/network-workspace/variables.tf
@@ -94,6 +94,7 @@ variable "aws_subnets_controller" {
     type = map(object({
       cidr_block = string
       availability_zone = string
+      map_public_ip_on_launch = bool
       aws_subnet_tag = map(string)
     }))
 
