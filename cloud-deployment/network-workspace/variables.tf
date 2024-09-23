1c5d01ea9470923868832455802732fa222cae6f
surawat-prem <tra.sruawat@gmail.com>
2024-09-24 03:10:56 +0700

    update utility subnet cidr

--- cloud-deployment/network-workspace/variables.tf
+++ cloud-deployment/network-workspace/variables.tf
@@ -78,7 +78,7 @@ variable "aws_subnets_workload" {
         }
       },
       k8s-utility-1 = {
-        "cidr_block" = "10.0.124.0/24"
+        "cidr_block" = "10.0.120.0/21"
         "availability_zone" = "ap-southeast-1a"
         "map_public_ip_on_launch" = true
         "aws_subnet_tag" = {
