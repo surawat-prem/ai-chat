e8858f366243e9456634c758654cf4bc4a9d5925
surawat-prem <tra.sruawat@gmail.com>
2024-09-27 17:38:49 +0700

    change policy to data

--- cloud-deployment/modules/aws_kms_vault/main.tf
+++ cloud-deployment/modules/aws_kms_vault/main.tf
@@ -7,25 +7,24 @@ resource "aws_kms_key" "vault" {
   }
 }
 
+data "aws_iam_policy_document" "vaukt-policy-data" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:GenerateDataKey",
+      "kms:GenerateDataKeyWithoutPlaintext"
+    ]
+    resources = aws_kms_key.vault.arn
+  }
+}
+
 resource "aws_iam_policy" "vault-kms-policy" {
   name        = "VaultKMSAccess"
   description = "Policy to allow Vault to access the KMS key"
 
-  policy = jsonencode({
-    Version = "2024-09-27"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "kms:Encrypt",
-          "kms:Decrypt",
-          "kms:GenerateDataKey",
-          "kms:GenerateDataKeyWithoutPlaintext",
-        ]
-        Resource = aws_kms_key.vault.arn
-      },
-    ]
-  })
+  policy = data.aws_iam_policy_document.vaukt-policy-data
 }
 
 resource "aws_iam_user" "vault-user" {
