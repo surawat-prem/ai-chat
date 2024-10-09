24cf2ad4cc7a1f22b0f551cdcce9160fdbe17c3e
surawat-prem <tra.sruawat@gmail.com>
2024-10-09 20:30:17 +0700

    init vault secret operator in non-prod namespace

--- ops-deployment/base/cnpg/charts/cnpg-cluster/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE messages (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    type TEXT NOT NULL,
-    user_id TEXT NOT NULL,
-    message TEXT NOT NULL,
-    timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
-);
