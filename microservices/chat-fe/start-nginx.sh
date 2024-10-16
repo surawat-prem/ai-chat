653b1dd07cf949f180eeb133b8b8e24314f5dce8
surawat-prem <tra.sruawat@gmail.com>
2024-10-16 22:01:19 +0700

    fix chat-fe env

--- microservices/chat-fe/start-nginx.sh
+++ microservices/chat-fe/start-nginx.sh
@@ -3,6 +3,10 @@
 # Replace placeholders in the default configuration
 HOSTNAME=${HOST:-localhost}
 PORT=${PORT:-80}
+CHAT_SERVER_SOCKET_URL=${CHAT_SERVER_SOCKET_URL:-localhost}
+
+envsubst '$$CHAT_SERVER_SOCKET_URL' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html.tmp \
+  && mv /usr/share/nginx/html/index.html.tmp /usr/share/nginx/html/index.html
 
 envsubst '$$HOSTNAME $$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
