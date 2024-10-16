13564cb5458a457975ef3d3d629b6bcab99a603b
surawat-prem <tra.sruawat@gmail.com>
2024-10-16 20:52:45 +0700

    trigger build

--- microservices/chat-backend/docker/docker-build.sh
+++ microservices/chat-backend/docker/docker-build.sh
@@ -1,4 +1,4 @@
 #Local build only
 TAG="v3"
 
-docker buildx build --platform linux/arm64,linux/amd64 -f docker/Dockerfile -t surawatprem/chat-backend:$TAG . --push
+docker buildx build --platform linux/arm64,linux/amd64 -f docker/Dockerfile -t surawatprem/chat-backend:$TAG .
