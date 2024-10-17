f1f5ed75f2db5250ce77efac531c8836c0fd0342
surawat-prem <tra.sruawat@gmail.com>
2024-10-17 12:17:56 +0700

    add manual deployment for certmanager secrets

--- ops-manual-deployment/manual-install.sh
+++ ops-manual-deployment/manual-install.sh
@@ -9,3 +9,6 @@ kubectl apply -n argocd -f ./argo-cd/cluster-app.yaml
 echo "Creating vault secrets"
 kubectl create ns vault
 kubectl apply -f ./vault/.secrets
+
+echo "Creating certmanager secrets"
+kubectl apply -f ./vault/.secrets
