6ec5a8d871dd82fb1e2f5a139cc723aabb310f84
surawat-prem <tra.sruawat@gmail.com>
2024-10-17 12:01:06 +0700

    fix generate secret scripts

--- ansible/generate-secrets.sh
+++ ansible/generate-secrets.sh
@@ -22,11 +22,32 @@ VPC_ID=$(aws ec2 describe-vpcs --filters Name=tag:Name,Values=workload --query '
 echo "VPC_ID: $VPC_ID" >> .secrets/kops-secrets.yaml
 echo "DONE: Getting vpc id for kops cluster"
 
-# Get subnet ids for kops cluster
-echo "Getting subnet ids for kops cluster"
-VPC_ID=$(aws ec2 describe-vpcs --filters Name=tag:Name,Values=workload --query 'Vpcs[0].VpcId')
-QUERY_SUBNET_IDS=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$VPC_ID --output json)
-SUBNET_IDS=$(echo "$QUERY_SUBNET_IDS" | jq -c -r '.Subnets[].SubnetId' | paste -sd, -)
-echo "SUBNET_IDS: $SUBNET_IDS" >> .secrets/kops-secrets.yaml
-echo "DONE: Getting subnet ids for kops cluster"
-echo "---Secret file generation completed---"
+# Get hosted zone id
+# echo "Getting hosted zone id for kops cluster"
+# TMP_ZONE=$(aws route53 list-hosted-zones --query "HostedZones[?contains(Name, 'surawattest.online.') && Config.PrivateZone == \`true\`].Id" | awk -F'/' '{print $NF}')
+# echo "HOSTED_ZONE_ID: $TMP_ZONE" >> .secrets/kops-secrets.yaml
+# echo "DONE: Getting hosted zone id for kops cluster"
+
+# echo "HOSTED_ZONE_ID: $(aws route53 list-hosted-zones --query "HostedZones[?Name == 'surawattest.online.'].Id" --output text)" >> .secrets/kops-secrets.yaml
+
+
+# # Get subnet ids for kops cluster
+# echo "Getting subnet ids for kops cluster"
+# VPC_ID=$(aws ec2 describe-vpcs --filters Name=tag:Name,Values=workload --query 'Vpcs[0].VpcId')
+# QUERY_SUBNET_IDS=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$VPC_ID Name=tag:SubnetType,Values=Private --output json)
+# SUBNET_IDS=$(echo "$QUERY_SUBNET_IDS" | jq -c -r '.Subnets[].SubnetId' | paste -sd, -)
+# echo "SUBNET_IDS: $SUBNET_IDS" >> .secrets/kops-secrets.yaml
+# echo "DONE: Getting subnet ids for kops cluster"
+# echo "---Secret file generation completed---"
+
+# # Get subnet ids for kops cluster
+# echo "Getting utility subnet ids for kops cluster"
+# VPC_ID=$(aws ec2 describe-vpcs --filters Name=tag:Name,Values=workload --query 'Vpcs[0].VpcId')
+# QUERY_SUBNET_IDS=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$VPC_ID Name=tag:SubnetType,Values=Public --output json)
+# UTILITY_SUBNET_IDS=$(echo "$QUERY_SUBNET_IDS" | jq -c -r '.Subnets[].SubnetId' | paste -sd, -)
+# echo "UTILITY_SUBNET_IDS: $UTILITY_SUBNET_IDS" >> .secrets/kops-secrets.yaml
+# echo "DONE: Getting utility subnet ids for kops cluster"
+# echo "---Secret file generation completed---"
+
+aws route53 list-hosted-zones --query "HostedZones[?Name == 'surawattest.online.' && Config.PrivateZone == 'true'].Id" --output text
+aws route53 list-hosted-zones --query "HostedZones[?Name == 'surawattest.online.'].Id" --output text
